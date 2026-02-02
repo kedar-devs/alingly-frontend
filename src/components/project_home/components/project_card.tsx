7568fc8ac185983ae07ae8e0309190896a46ddb7
Devasthali-Kedar_bcgprod <Devasthali.Kedar@bcg.com>
2026-02-02 11:35:47 +0530

    Refactor ProjectDataCard component to improve layout and styling; update button functionality for visiting projects and enhance image display with rounded corners.

--- src/components/project_home/components/project_card.tsx
+++ src/components/project_home/components/project_card.tsx
@@ -6,30 +6,25 @@ type ProjectDataCardProps = {
 }
 
 function ProjectDataCard({ projectCard, onVisit }: ProjectDataCardProps) {
-  const handleVisit = () => onVisit?.(projectCard.project_id)
 
   return (
     <div className=" w-80 h-80 border rounded-sm border-[#5bd787] text-[#5bd787] flex flex-col justify-center items-center p-5">
-      <img src="/project_main.jpg" alt="Project Image" className=" w-full h-1/3 object-cover" />
-      <div className=" w-full grid grid-cols-5 gap-y-5">
-        <div className=" col-span-2">
-            Project Name:
-        </div>
+      <img src="/project_main.jpg" alt="Project Image" className=" w-full h-1/3 object-cover border-0 rounder-lg" />
+      <div className=" w-full flex flex-col gap-y-5">
         <div className="col-span-3">
           {projectCard.name}
-        </div>
-        <div className=" col-span-2">
-            Project Description:
         </div>
         <div className="col-span-3 ">
           {(projectCard.description?.slice(0, 50) ?? "")}{(projectCard.description?.length ?? 0) > 50 ? "..." : ""}
         </div>
-        <button className="w-full h-10 bg-[#5bd787] text-black rounded-md cursor-pointer font-bold" onClick={()=>{
-          navigateToProject(projectCard.project_id)
+        <div className="w-full flex items-center justify-center">
+        <button className="w-1/2 h-10 bg-[#5bd787] text-black rounded-md cursor-pointer font-bold" onClick={()=>{
+          onVisit?.(projectCard.project_id)
         }
         }>
-          Visit
-        </button>
+            Visit
+          </button>
+        </div>
       </div>
 
     </div>
